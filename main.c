698f16cb7a379c982f07c6489d063236f1e73c4d
tinhbuiv <tinhbvth1809016@fpt.edu.vn>
2018-10-10 21:20:00 +0700

    sua bai 2

--- main.c
+++ main.c
@@ -3,77 +3,33 @@
 
 
 int main() {
-    int kinhnghiem;
-    printf("Nhap vao so nam kinh nghiem :\n");
-    scanf("%d", &kinhnghiem);
-    int sonamlamviec;
-    printf("Nhap vao so nam lam viec tai cong ty\n");
-    scanf("%d", &sonamlamviec);
-    //Cac truong hop voi so nam kinh nghiem < 2
-    if (kinhnghiem < 2 && sonamlamviec < 1) {
-        printf("Luong thuong thang 13 cua ban la 3 trieu VND ");
-        return 0;
+    int a, b;
+    printf("Vui long nhap so nam kinh nghiem");
+    scanf("%d", &a);
+    printf("Vui long so nam lam viec cho cong ty");
+    scanf("%d", &b);
 
-    } else if (kinhnghiem < 2 && 1 <= sonamlamviec && sonamlamviec < 2) {
-        printf("Luong thuong thang 13 cua ban la 5 trieu VND ");
-        return 0;
-    } else if (kinhnghiem < 2 && 2 <= sonamlamviec && sonamlamviec <= 5) {
-        printf("Luong thuong thang 13 cua ban la 10 trieu VND ");
-        return 0;
-    } else if (kinhnghiem < 2 && 5 < sonamlamviec) {
-        printf("Luong thuong thang 13 cua ban la 20 trieu VND ");
-        return 0;
+    int luongchinhthuc = 0;
+    float thuongthang13 = 0;
+    if (a < 2) {
+        printf("Luong chinh thuc trong 12 thang chinh la 10 trieu");
+        luongchinhthuc = 10;
+    } else if (a >= 2 && a <= 5) {
+        printf("Luong chinh thuc trong 12 thang chinh la 20 trieu");
+        luongchinhthuc = 20;
+    } else {
+        printf("\nLuong chinh thuc trong 12 thang chinh la 30 trieu\n");
+        luongchinhthuc = 30;
     }
-// cac truong hop voi so nam kinh nghiem tu 2 den 5 nam
-    if (2 <= kinhnghiem && kinhnghiem <= 5 && sonamlamviec < 1) {
-        printf("Luong thuong thang 13 cua ban la 6 trieu VND ");
-        return 0;
-
-    } else if (2 <= kinhnghiem && kinhnghiem <= 5 && 1 <= sonamlamviec  && sonamlamviec < 2) {
-        printf("Luong thuong thang 13 cua ban la 10 trieu VND ");
-        return 0;
-    } else if (2 <= kinhnghiem && kinhnghiem <= 5 && 2 <= sonamlamviec && sonamlamviec <= 5) {
-        printf("Luong thuong thang 13 cua ban la 20 trieu VND ");
-        return 0;
-    } else if (2 <= kinhnghiem && kinhnghiem <= 5 && 5 < sonamlamviec) {
-        printf("Luong thuong thang 13 cua ban la 40 trieu VND ");
-        return 0;
+    if (b < 1) {
+        thuongthang13 = luongchinhthuc * 0.3;
+    } else if (b >= 1 & b <= 2) {
+        thuongthang13 = luongchinhthuc * 0.5;
+    } else if (b >= 2 && b <= 5) {
+        thuongthang13 = luongchinhthuc;
+    } else {
+        thuongthang13 = luongchinhthuc * 2;
     }
-//cac truong hop voi so nam kinh nghiem tren 5 nam
-    if (5 < kinhnghiem && sonamlamviec < 1) {
-        printf("Luong thuong thang 13 cua ban la 9 trieu VND ");
-        return 0;
-
-    } else if (5 < kinhnghiem && 1 <= sonamlamviec && sonamlamviec < 2) {
-        printf("Luong thuong thang 13 cua ban la 15 trieu VND ");
-        return 0;
-    } else if (5 < kinhnghiem && 2 <= sonamlamviec && sonamlamviec <= 5) {
-        printf("Luong thuong thang 13 cua ban la 30 trieu VND ");
-        return 0;
-    } else if (5 < kinhnghiem && 5 < sonamlamviec) {
-        printf("Luong thuong thang 13 cua ban la 60 trieu VND ");
-        return 0;
-    }
+    printf("\nluong thuong thang 13 cua ban la %f\n", thuongthang13);
+    return 0;
 }
-
-
-
-//    if (kinhnghiem < 2 && 1 <= sonamlamviec < 2) {
-//        printf("Luong thuong thang 13 cua ban la 5 trieu VND");
-//    }
-//    if( 2 <= kinhnghiem <= 5 && 2 <= sonamlamviec <= 5)
-//    {
-//        printf("Luong thuong thang 13 cua ban la 20 trieu VND");
-//    }
-//    if(kinhnghiem > 5 && sonamlamviec > 5)
-//    {
-//        printf("Luong thuong thang 13 cua ban la 60 trieu");
-//    }
-//    if (kinhnghiem < 2) {
-//        switch (sonamlamviec) {
-//            case 1:
-//                printf("Luong thang 13 cua ban la 3tr");
-//                break;
-//            case 2:
-//        }
-//    }
